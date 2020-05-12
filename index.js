b740889d130a4585212edd3de307e3897aa95dd8
williamterry-dotcom <wt30749@gmail.com>
2020-05-12 15:00:04 -0400

    1 file modified

--- index.js
+++ index.js
@@ -16,7 +16,8 @@ class App extends Component {
       <div>
         <Hello name={this.state.name} />
         <p>
-          Start editing to see some magic happen :)
+          This is progress
+          :)
         </p>
       </div>
     );
